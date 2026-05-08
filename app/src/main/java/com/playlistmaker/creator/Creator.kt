82d2a8e4fb54c71deabd6cdbdfab3f749e37ded9
sasha-zadiraka <aleksandra.zadiraka@gmail.com>
2026-05-08 14:39:49 +0200

    fix after review

--- app/src/main/java/com/playlistmaker/creator/Creator.kt
+++ app/src/main/java/com/playlistmaker/creator/Creator.kt
@@ -6,23 +6,28 @@ import com.google.gson.Gson
 import com.playlistmaker.player.data.PlayerRepositoryImpl
 import com.playlistmaker.player.domain.PlayerInteractor
 import com.playlistmaker.player.domain.PlayerInteractorImpl
-import com.playlistmaker.util.AppConstants.PREFS_NAME
-import com.playlistmaker.search.data.network.NetworkClient
-import com.playlistmaker.search.data.network.ItunesApi
+import com.playlistmaker.player.domain.PlayerRepository
 import com.playlistmaker.search.data.SearchHistoryRepositoryImpl
-import com.playlistmaker.settings.data.ThemeRepositoryImpl
 import com.playlistmaker.search.data.TracksRepositoryImpl
-import com.playlistmaker.sharing.data.ExternalNavigatorImpl
+import com.playlistmaker.search.data.network.ItunesApi
+import com.playlistmaker.search.data.network.NetworkClient
 import com.playlistmaker.search.domain.SearchHistoryInteractor
-import com.playlistmaker.sharing.domain.SharingInteractor
-import com.playlistmaker.settings.domain.ThemeInteractor
-import com.playlistmaker.search.domain.TracksInteractor
 import com.playlistmaker.search.domain.SearchHistoryInteractorImpl
-import com.playlistmaker.sharing.domain.SharingInteractorImpl
-import com.playlistmaker.settings.domain.ThemeInteractorImpl
+import com.playlistmaker.search.domain.SearchHistoryRepository
+import com.playlistmaker.search.domain.TracksInteractor
 import com.playlistmaker.search.domain.TracksInteractorImpl
+import com.playlistmaker.search.domain.TracksRepository
+import com.playlistmaker.settings.data.ThemeRepositoryImpl
+import com.playlistmaker.settings.domain.ThemeInteractor
+import com.playlistmaker.settings.domain.ThemeInteractorImpl
+import com.playlistmaker.settings.domain.ThemeRepository
+import com.playlistmaker.sharing.data.ExternalNavigatorImpl
+import com.playlistmaker.sharing.domain.ExternalNavigator
+import com.playlistmaker.sharing.domain.SharingInteractor
+import com.playlistmaker.sharing.domain.SharingInteractorImpl
 import com.playlistmaker.sharing.domain.models.EmailData
 import com.playlistmaker.sharing.domain.models.SharingData
+import com.playlistmaker.util.AppConstants.PREFS_NAME
 
 object Creator {
 
@@ -30,7 +35,7 @@ object Creator {
         return NetworkClient.retrofit.create(ItunesApi::class.java)
     }
 
-    private fun provideTracksRepository(): TracksRepositoryImpl {
+    private fun provideTracksRepository(): TracksRepository {
         return TracksRepositoryImpl(provideItunesApi())
     }
 
@@ -38,7 +43,7 @@ object Creator {
         return TracksInteractorImpl(provideTracksRepository())
     }
 
-    private fun provideSearchHistoryRepository(context: Context): SearchHistoryRepositoryImpl {
+    private fun provideSearchHistoryRepository(context: Context): SearchHistoryRepository {
         return SearchHistoryRepositoryImpl(
             sharedPreferences = context.getSharedPreferences("search_prefs", Context.MODE_PRIVATE),
             gson = Gson()
@@ -49,7 +54,7 @@ object Creator {
         return SearchHistoryInteractorImpl(provideSearchHistoryRepository(context))
     }
 
-    private fun provideThemeRepository(context: Context): ThemeRepositoryImpl {
+    private fun provideThemeRepository(context: Context): ThemeRepository {
         return ThemeRepositoryImpl(
             sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         )
@@ -59,9 +64,11 @@ object Creator {
         return ThemeInteractorImpl(provideThemeRepository(context))
     }
 
-    fun provideSharingInteractor(context: Context): SharingInteractor {
-        val externalNavigator = ExternalNavigatorImpl(context)
+    private fun provideExternalNavigator(context: Context): ExternalNavigator {
+        return ExternalNavigatorImpl(context)
+    }
 
+    fun provideSharingInteractor(context: Context): SharingInteractor {
         val sharingData = SharingData(
             shareAppLink = context.getString(R.string.course_link),
             userAgreementLink = context.getString(R.string.user_agreement_link)
@@ -74,13 +81,17 @@ object Creator {
         )
 
         return SharingInteractorImpl(
-            externalNavigator = externalNavigator,
+            externalNavigator = provideExternalNavigator(context),
             sharingData = sharingData,
             emailData = emailData
         )
     }
 
+    private fun providePlayerRepository(): PlayerRepository {
+        return PlayerRepositoryImpl()
+    }
+
     fun providePlayerInteractor(): PlayerInteractor {
-        return PlayerInteractorImpl(PlayerRepositoryImpl())
+        return PlayerInteractorImpl(providePlayerRepository())
     }
 }
